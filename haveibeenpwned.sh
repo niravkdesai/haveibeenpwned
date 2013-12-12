8910403fcacc4f689a8363fd304200f319c68b5f
niravkdesai <nirav.desai1991@gmail.com>
2013-12-12 21:32:32 +0530

    Create haveibeenpwned.sh

---
+++ haveibeenpwned.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash 
+
+
+echo  "Please enter email address to check against http://haveibeenpwned.com"
+
+read choice
+response=$(curl --write-out %{http_code} --silent --output /dev/null "http://haveibeenpwned.com/api/breachedaccount/$choice")
+
+if [ "$response" = "404" ];then
+    echo "Congo..You are safe "
+
+elif [ "$response" = "400" ];then
+    echo "Upps...Something went wrong."
+ 
+  
+else
+    temp=$(curl --silent --request GET "http://haveibeenpwned.com/api/breachedaccount/$choice")
+    echo  "You have been pwned in "$temp" breach "
+    
+fi
