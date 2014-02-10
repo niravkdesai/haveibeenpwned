a398a815f80c604ee7e04c3d8e603fffcf6e1f02
Nirav <nirav.desai1991@gmail.com>
2014-02-10 20:04:20 +0530

    Update haveibeenpwned.sh

--- haveibeenpwned.sh
+++ haveibeenpwned.sh
@@ -18,15 +18,17 @@ else
     echo  "You have been pwned in "$temp" breach "
     echo  "Do you require further Details?(y/n)"
     read ans
-    
     if [ "$ans" = "y" ] || [ "$ans" = "Y" ] ;then
-	    curl --silent --request GET "https://haveibeenpwned.com/api/v2/breachedaccount/$choice"|python -mjson.tool > $choice.txt
-	    cat $choice.txt
-	    echo "Output saved it into $choice.txt file"
+	curl --silent --request GET "https://haveibeenpwned.com/api/v2/breachedaccount/$choice"|python -mjson.tool > $choice.txt
+	cat $choice.txt|sed -e 's!http\(s\)\{0,1\}://[^[:space:]]*!!g' -e 's/[@#\$%^&*()=039"]//g' -e 's/<\/td>//g' -e 's/<em>//g' -e 's/<\/em>//g' -e 's/<a//g'  -e 's/<\/a>//g'  >  Breach_$choice.txt
+	cat Breach_$choice.txt
+	rm $choice.txt
+
+	echo "Output saved it into Breach_$choice.txt file"
     fi
 
     if [ "$ans" = "n" ] || [ "$ans" = "N" ] ;then 	
-    	exit 1
+	exit 1
     fi
 	    
 fi
