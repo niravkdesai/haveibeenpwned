65a3f988617697b44855bb3dc8bf8058ab300ba0
Nirav <nirav.desai1991@gmail.com>
2014-02-10 19:49:22 +0530

    Update haveibeenpwned.sh
    apiv2 added

--- haveibeenpwned.sh
+++ haveibeenpwned.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash 
 
 
-echo  "Please enter email address to check against http://haveibeenpwned.com"
+echo  "Please enter email address to check against https://haveibeenpwned.com"
 
 read choice
 response=$(curl --write-out %{http_code} --silent --output /dev/null "https://haveibeenpwned.com/api/breachedaccount/$choice")
 
 if [ "$response" = "404" ];then
-    echo "Congo..You are safe "
+    echo "Congralaution!!You are safe "
 
 elif [ "$response" = "400" ];then
-    echo "Upps...Something went wrong."
+    echo "Upps.Something went wrong." 
  
   
 else
     temp=$(curl --silent --request GET "https://haveibeenpwned.com/api/breachedaccount/$choice")
     echo  "You have been pwned in "$temp" breach "
+    echo  "Do you require further Details?(y/n)"
+    read ans
     
+    if [ "$ans" = "y" ] || [ "$ans" = "Y" ] ;then
+	    curl --silent --request GET "https://haveibeenpwned.com/api/v2/breachedaccount/$choice"|python -mjson.tool > $choice.txt
+	    cat $choice.txt
+	    echo "Output saved it into $choice.txt file"
+    fi
+
+    if [ "$ans" = "n" ] || [ "$ans" = "N" ] ;then 	
+    	exit 1
+    fi
+	    
 fi
